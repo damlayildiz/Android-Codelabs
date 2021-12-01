dc8365ee02156890a0bbfd7a685d6762d0217bd3
damlayildiz <damlayildiz19@ku.edu.tr>
2021-12-01 19:56:12 +0300

    Add TestingBasics

---
+++ TestingBasics/app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
@@ -0,0 +1,50 @@
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.android.architecture.blueprints.todoapp.tasks.TasksFilterType
+import com.example.android.architecture.blueprints.todoapp.tasks.TasksViewModel
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.not
+import org.hamcrest.Matchers.nullValue
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class TasksViewModelTest {
+
+	// Subject under test
+	private lateinit var tasksViewModel: TasksViewModel
+
+	@Before
+	fun setupViewModel() {
+		tasksViewModel = TasksViewModel(ApplicationProvider.getApplicationContext())
+	}
+
+	@get:Rule
+	var instantExecutorRule = InstantTaskExecutorRule()
+
+
+	@Test
+	fun addNewTask_setsNewTaskEvent() {
+		// When adding a new task
+		tasksViewModel.addNewTask()
+
+		// Then the new task event is triggered
+		val value = tasksViewModel.newTaskEvent.getOrAwaitValue()
+
+		assertThat(value.getContentIfNotHandled(), not(nullValue()))
+	}
+
+	@Test
+	fun setFilterAllTasks_tasksAddViewVisible() {
+		// When the filter type is ALL_TASKS
+		tasksViewModel.setFiltering(TasksFilterType.ALL_TASKS)
+
+		// Then the "Add task" action is visible
+		assertThat(tasksViewModel.tasksAddViewVisible.getOrAwaitValue(), `is`(true))
+	}
+
+}
